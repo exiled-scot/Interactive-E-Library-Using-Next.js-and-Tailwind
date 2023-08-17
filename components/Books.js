98e9d1ed6c517cd046d9adb55c8291b81ab21a3c
armitage <scott@nihilanth.co.uk>
2023-08-17 13:14:49 +0100

    search bar

--- components/Books.js
+++ components/Books.js
@@ -1,16 +1,22 @@
 import Book from './Book';
 
-const Books = ({ data }) => {
+const Books = ({ data, searchString }) => {
+    const checkCondition = (elem) => {
+        return elem.toLowerCase().includes(searchString.toLowerCase());
+    }
 
     return (
         <section className="grid justify-items-center gap-4">
         {
             data.map((book, index) => {
-                return (
-                    <div key={index} className="border border-gray-100 mt-1">
-                        <Book book={book} />
-                    </div>
-                )
+                if (checkCondition(book.title) || checkCondition(book.author)) {
+                    return (
+                        <div key={index} className="border border-gray-100 mt-1">
+                            <Book book={book} />
+                        </div>
+                    )
+                }
+                else return <></>
             })
         }
         </section>
