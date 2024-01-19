7813f514c62b974590e67c1721428cddd5ab01a3
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-19 23:50:30 +0000

    better spacing

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="px-4 py-6">
-      <div className="flex justify-center">
+    <footer className="px-4 py-6 text-center">
+      <div className="mx-20 flex justify-center">
         {/* Column 1 */}
         <div className="w-1/4">
-          <h3 className="text-lg mb-2">COMPANY</h3>
-          <ul className="text-sm">
+          <h3 className="text-left text-lg mb-2">COMPANY</h3>
+          <ul className="text-left text-sm">
             <li><a href="#">About us</a></li>
             <li><a href="#">Careers</a></li>
             <li><a href="#">Terms</a></li>
@@ -18,8 +18,8 @@ const Footer = () => {
         
         {/* Column 2 */}
         <div className="w-1/4">
-          <h3 className="text-lg mb-2">WORK WITH US</h3>
-          <ul className="text-sm">
+          <h3 className="text-left text-lg mb-2">WORK WITH US</h3>
+          <ul className="text-left text-sm">
             <li><a href="#">Authors</a></li>
             <li><a href="#">Advertise</a></li>
             <li><a href="#">Authors &amp; ads blog</a></li>
@@ -29,8 +29,8 @@ const Footer = () => {
         
         {/* Column 3 */}
         <div className="w-1/4">
-          <h3 className="text-lg mb-2">CONNECT</h3>
-          <ul className="text-sm">
+          <h3 className="text-left text-lg mb-2">CONNECT</h3>
+          <ul className="text-left text-sm">
             <li><a href="#">Goodreads on Facebook</a></li>
             <li><a href="#">Goodreads on Twitter</a></li>
             <li><a href="#">Goodreads on Instagram</a></li>
@@ -40,15 +40,15 @@ const Footer = () => {
         
         {/* Column 4 */}
         <div className="w-1/4">
-          <h3 className="text-lg mb-2">Download the app</h3>
-          <ul className="text-sm">
+          <h3 className="text-left text-lg mb-2">Download the app</h3>
+          <ul className="text-left text-sm">
               <li><a href="#">Download app for iOS</a></li>
               <li><a href="#">Download app for Android</a></li>
           </ul>
         </div>
       </div>
       
-      <div className="flex justify-center mt-4">
+      <div className="mt-4">
         <p className="text-xs">&copy; 2024 MyReads</p>
       </div>
     </footer>
