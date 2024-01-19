e96e068f05fea18e7904576637273ce7d4fa6004
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-19 12:26:15 +0000

    outline-none to search bar

--- components/Header.js
+++ components/Header.js
@@ -24,15 +24,28 @@ const Header = ({ searchString, handleSearch }) => {
           <a href="" className="text-2xl my-4 mx-4 text-white">Browse</a>
           <a href="" className="text-2xl my-4 mx-4 text-white">Community</a>
         </div>
-        <div className="">
-          <input
-            type="text"
-            placeholder="Search books"
-            value={searchString}
-            onChange={handleSearch}
-            ref={inputRef}
-          />
+
+        <div class='max-w-md mx-auto flex justify-center'>
+          <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+            <div class="grid place-items-center h-full w-20 text-gray-300">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </div>
+
+            <input
+              type="text"
+              placeholder="Search books"
+              value={searchString}
+              onChange={handleSearch}
+              ref={inputRef}
+              class="pl-4 outline-none"
+              style={{ width: "100vw", border: "none"}}
+            />
+          </div>
         </div>
+
+
         <div className="flex justify-end items-center pr-6 text-gray-300">
           <div>
             <button className="text-2xl mx-4 mr-4 text-white">Log in</button>
