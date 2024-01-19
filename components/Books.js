817c4902336ff7e411da1f495e6d45a337ed154a
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-19 22:50:02 +0000

    tiles

--- components/Books.js
+++ components/Books.js
@@ -1,26 +1,28 @@
 import Book from './Book';
 
 const Books = ({ data, searchString }) => {
-    const checkCondition = (elem) => {
-        return elem.toLowerCase().includes(searchString.toLowerCase());
-    }
+  const checkCondition = (elem) => {
+    return elem.toLowerCase().includes(searchString.toLowerCase());
+  }
 
-    return (
-        <section className="grid justify-items-center gap-4">
+  return (
+    <section className="justify-items-center auto-cols-fr" style={{gridGap: '.75em', padding: '.75em'}}>
+      <div className="flex flex-wrap justify-between gap-4">
         {
-            data.map((book, index) => {
-                if (checkCondition(book.title) || checkCondition(book.author)) {
-                    return (
-                        <div key={index} className="border border-gray-100 mt-1">
-                            <Book book={book} />
-                        </div>
-                    )
-                }
-                else return <></>
-            })
+          data.map((book, index) => {
+            if (checkCondition(book.title) || checkCondition(book.author)) {
+              return (
+                <div key={index} className="border border-gray-100 mt-1">
+                  <Book book={book} />
+                </div>
+              )
+            }
+            else return <></>
+          })
         }
-        </section>
-    )
+      </div>
+    </section>
+  )
 }
 
 export default Books;
