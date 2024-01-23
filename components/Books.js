7cfeb1d48165583472d24536e6e91fa7e7d6d1bc
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-23 18:32:50 +0000

    responsive mobile

--- components/Books.js
+++ components/Books.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import Book from './Book';
 
 const Books = ({ data, searchString }) => {
@@ -5,8 +6,40 @@ const Books = ({ data, searchString }) => {
     return elem.toLowerCase().includes(searchString.toLowerCase());
   }
 
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1024);
+    };
+    
+    handleResize();
+    
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <section className="mx-40 justify-items-center auto-cols-fr" style={{gridGap: '.75em', padding: '.75em'}}>
+    <section
+      className={`overflow-auto justify-items-center auto-cols-fr${isLargeScreen ? ' mx-40' : ''}`}
+      style={{
+        gridGap: '.75em',
+        padding: '.75em',
+        overflow: 'hidden',
+        scrollbarWidth: 'thin',
+        scrollbarColor: 'transparent transparent',
+        '&::-webkit-scrollbar': {
+          width: '0.5em',
+          height: '0.5em',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: 'transparent',
+        },
+      }}
+    >
       <div className="flex flex-wrap gap-4">
         {
           data.map((book, index) => {
@@ -16,8 +49,7 @@ const Books = ({ data, searchString }) => {
                   <Book book={book} />
                 </div>
               )
-            }
-            else return <></>
+            } else return <></>
           })
         }
       </div>
