197f704494653d81b32af5ed32eff1c8861c2885
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-25 15:38:50 +0000

    added link

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,21 +9,26 @@ const getCurrentTheme = () => {
 const Sidebar = ({ data }) => {
   const currentTheme = getCurrentTheme();
 
-  const {
-    title,
-    sections,
-    promotionText
-  } = data;
+  const { title, sections, promotionText } = data;
 
   return (
-    <div className={`${currentTheme === "dark" ? "bg-gray-800" : "bg-white"} text-gray-80 max-w-[24rem] whitespace-normal break-words rounded-lg border border-blue-gray-50 p-4 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none max-w-xs bg-gray-100 p-4`} style={{ zIndex: 9999 }}>
+    <div
+      className={`${currentTheme === "dark" ? "bg-gray-800" : "bg-white"
+        } text-gray-80 max-w-[24rem] whitespace-normal break-words rounded-lg border border-blue-gray-50 p-4 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none max-w-xs bg-gray-100 p-4`}
+      style={{ zIndex: 9999 }}
+    >
       <div className="font-bold text-lg">{title}</div>
       {sections.map((section, index) => (
         <div key={index}>
           <div className="font-bold my-2">{section.title}</div>
           {section.content.map((content, contentIndex) => (
             <div key={contentIndex} className="my-2">
-              {content}
+              <a href={content.link} className={`${currentTheme === "dark" ? "bg-gray-800" : "bg-white"
+                } italic`}>
+                {content.title}
+              </a>
+              <div className="inline"> by </div>
+              <div className="inline">{content.author}</div>
             </div>
           ))}
           {section.likes && section.comments && (
