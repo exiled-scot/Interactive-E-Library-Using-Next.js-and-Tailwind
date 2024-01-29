88b5e12d498c5d0924453818e18da076b315cd89
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-29 09:57:12 +0000

    margin

--- components/Reviews.js
+++ components/Reviews.js
@@ -7,25 +7,27 @@ const Reviews = ({ reviews }) => {
 
     return (
         <div>
-            <h2 className="text-2xl font-bold">Reviews</h2>
-            <ReviewTable reviews={reviews} />
-            <ul>
-                {reviewsArray?.map((review, index) => (
-                    <li key={index}>
-                        <div className="flex items-center mb-0">
-                            <h3 className="mr-2 font-bold">{review.title}</h3>
-                            <StarRatings
-                                rating={review.rating}
-                                starRatedColor="blue"
-                                starDimension="20px"
-                                starSpacing="2px"
-                            />
-                        </div>
-                        <p className="text-grey font-italic">{review.author}</p>
-                        <p>{review.description}</p>
-                    </li>
-                ))}
-            </ul>
+            <div className="mx-4">
+                <h2 className="text-2xl font-bold">Reviews</h2>
+                <ReviewTable reviews={reviews} />
+                <ul>
+                    {reviewsArray?.map((review, index) => (
+                        <li key={index}>
+                            <div className="flex items-center mb-0">
+                                <h3 className="mr-2 font-bold">{review.title}</h3>
+                                <StarRatings
+                                    rating={review.rating}
+                                    starRatedColor="blue"
+                                    starDimension="20px"
+                                    starSpacing="2px"
+                                />
+                            </div>
+                            <p className="text-grey font-italic">{review.author}</p>
+                            <p>{review.description}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
