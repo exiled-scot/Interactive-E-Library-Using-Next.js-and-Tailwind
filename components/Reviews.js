71f182d388323acad32894866472f7760bc64c36
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-29 10:23:00 +0000

    spacing of reviews

--- components/Reviews.js
+++ components/Reviews.js
@@ -11,10 +11,10 @@ const Reviews = ({ reviews }) => {
                 <h2 className="text-2xl font-bold">Reviews</h2>
                 <ReviewTable reviews={reviews} />
                 <br/>
-                <ul>
+                <ul className="mt-4">
                     {reviewsArray?.map((review, index) => (
-                        <li key={index}>
-                            <div className="flex items-center mb-0">
+                        <li key={index} className="mb-8">
+                            <div className="flex items-center mb-2">
                                 <h3 className="mr-2 font-bold">{review.title}</h3>
                                 <StarRatings
                                     rating={review.rating}
@@ -23,8 +23,8 @@ const Reviews = ({ reviews }) => {
                                     starSpacing="2px"
                                 />
                             </div>
-                            <p className="text-grey font-italic">{review.author}</p>
                             <p>{review.description}</p>
+                            <p className="text-grey font-italic">- {review.author}</p>
                         </li>
                     ))}
                 </ul>
