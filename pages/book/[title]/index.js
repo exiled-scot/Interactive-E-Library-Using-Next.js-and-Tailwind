b2f521158d6f1748efe699d88e5c6503a0c34c98
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-29 11:14:23 +0000

    /book/doesnotexist

--- pages/book/[title]/index.js
+++ pages/book/[title]/index.js
@@ -5,9 +5,9 @@ import Link from "next/link";
 import { FaStar } from "react-icons/fa";
 import { useRouter } from "next/router";
 import Reviews from "../../../components/Reviews";
+import Custom404 from "../../404";
 
 const Book = () => {
-
   const reviews = {
     reviews: [
       {
@@ -22,7 +22,6 @@ const Book = () => {
         author: "John Doe",
         description: "This course was really helpful and informative. I highly recommend it.",
       },
-
       {
         title: "Well-explained content",
         rating: 5,
@@ -35,44 +34,43 @@ const Book = () => {
   const router = useRouter();
   const { title } = router.query;
   const [book, setBook] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const selectedBook = data.find((elem) => elem.title === title);
     setBook(selectedBook);
+    setIsLoading(false); // Set isLoading to false after the book data is fetched and set
   }, [title]);
 
   const StarRating = ({ rating }) => {
     const filledStars = Math.floor(rating); // Get the integer part of the rating
 
     // Create an array of SVG elements representing filled stars
-    const stars = Array.from({ length: filledStars }, (_, index) => (
-      <svg
+    const filledStarIcons = Array.from({ length: filledStars }, (_, index) => (
+      <FaStar
         key={index}
         className="w-6 h-6 ms-2 text-yellow-300"
         aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        viewBox="0 0 22 20"
-      >
-        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-      </svg>
+      />
+    ));
+
+    // Create an array of SVG elements representing empty stars
+    const emptyStarIcons = Array.from({ length: 5 - filledStars }, (_, index) => (
+      <FaStar
+        key={index + filledStars}
+        className="w-6 h-6 ms-2 text-gray-300 dark:text-gray-500"
+        aria-hidden="true"
+      />
     ));
 
     // Render the div element with the stars
     return (
       <div className="py-4 flex items-center">
-        {stars}
-        {/* Empty stars */}
-        <svg
-          className="w-6 h-6 ms-2 text-gray-300 dark:text-gray-500"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 22 20"
-        >
-          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-        </svg>
-        <div className="ml-4 font-bold text-xl ml-2">{book?.rating}</div>
+        {/* Render filled stars */}
+        {filledStarIcons}
+        {/* Render empty stars */}
+        {emptyStarIcons}
+        <div className="ml-4 font-bold text-xl ml-2">{rating}</div>
       </div>
     );
   };
@@ -84,41 +82,41 @@ const Book = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {book && (
+      {book && !isLoading ? (
         <div className="flex flex-col items-center py-12">
           <div className="max-w-4xl md:h-full">
             <div className="flex flex-col justify-center text-left px-4 md:px-0">
               <div className="grid md:grid-cols-2 md:gap-10">
                 {/* Right column - Image */}
                 <div className="md:order-1">
-                  <img className="w-full" src={book?.img} alt="Book Cover" />
+                  <img className="w-full" src={book.img} alt="Book Cover" />
                   <br />
                 </div>
 
                 {/* Left column - Text */}
                 <div className="md:order-0">
                   <h1 className="font-bold text-3xl md:text-4xl mb-2 text-left">
-                    {book?.title}
+                    {book.title}
                   </h1>
                   <h2 className="font-semibold mb-2 text-left">
-                    {book?.author}
+                    {book.author}
                   </h2>
 
-                  <StarRating rating={book?.rating} />
+                  <StarRating rating={book.rating} />
 
                   <p className="font-medium mb-2 text-left">
-                    {book?.overview}
+                    {book.overview}
                   </p>
                   <br />
 
                   <p className="font-medium mb-2 text-left">
-                    Published in: {book?.date}
+                    Published in: {book.date}
                   </p>
                   <p className="font-medium mb-2 text-left">
-                    Genre: {book?.genre.join(", ")}
+                    Genre: {book.genre.join(", ")}
                   </p>
                   <p className="font-medium mb-2 text-left">
-                    Tags: {book?.tags.join(", ")}
+                    Tags: {book.tags.join(", ")}
                   </p>
                   <br />
                   <div className="mt-6 about-author">
@@ -127,21 +125,23 @@ const Book = () => {
                     <div className="mt-4 flex items-center">
                       <div className="w-20 h-20 rounded-full bg-red-500 flex items-center justify-center">
                         <span className="text-white font-bold">
-                          {book?.author[0]}
+                          {book.author[0]}
                         </span>
                       </div>
                       <h3 className="ml-6 mt-1.5 text-lg font-semibold">
-                        {book?.author}
+                        {book.author}
                       </h3>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <br/>
+            <br />
             <Reviews reviews={reviews} />
           </div>
         </div>
+      ) : (
+        <Custom404/>
       )}
     </>
   );
