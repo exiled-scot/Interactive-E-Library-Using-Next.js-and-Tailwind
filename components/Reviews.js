ad1c299a66b4ab707866268853b43043ba41d017
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-29 11:15:44 +0000

    yellow stars

--- components/Reviews.js
+++ components/Reviews.js
@@ -18,7 +18,7 @@ const Reviews = ({ reviews }) => {
                                 <h3 className="mr-2 font-bold">{review.title}</h3>
                                 <StarRatings
                                     rating={review.rating}
-                                    starRatedColor="yellow-600"
+                                    starRatedColor="yellow"
                                     starDimension="20px"
                                     starSpacing="2px"
                                 />
